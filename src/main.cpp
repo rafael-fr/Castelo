a450f168eee7aa07585cba4106f4fa00d4d71e73
mmlucasbr <rafaelfr.projetos@gmail.com>
2026-06-16 17:26:10 -0300

    novas cenas celeste

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <Arduino.h>
 #include <Servo.h>
-// #include <WTV020SD16P.h>
+#include <WTV020SD16P.h>
 
-#include <DFRobotDFPlayerMini.h>
-#include <SoftwareSerial.h>
+// #include <DFRobotDFPlayerMini.h>
+// #include <SoftwareSerial.h>
 
-#include <porteiro/porteiro1.h>
-#include <porteiro/porteiro2.h>
-#include <porteiro/porteiro3.h>
-#include <porteiro/porteiro4.h>
-#include <porteiro/porteiro5.h>
-#include <porteiro/porteiro6.h>
-// // Defina os pinos para os servos
-const int pinOlho = 11;     //porteiro 
-const int pinBoca = 9;    //porteiro 
-const int pinPescoco = 10; //porteiro 
+// #include <porteiro/porteiro1.h>
+// #include <porteiro/porteiro2.h>
+// #include <porteiro/porteiro3.h>
+// #include <porteiro/porteiro4.h>
+// #include <porteiro/porteiro5.h>
+// #include <porteiro/porteiro6.h>
+// // // Defina os pinos para os servos
+// const int pinOlho = 11;     //porteiro 
+// const int pinBoca = 9;    //porteiro 
+// const int pinPescoco = 10; //porteiro 
 
-const int angulos_default[3] = {50,90,90};
+// const int angulos_default[3] = {50,90,90};
 
 // #include <porteiro2/porteiro1.h>
 // #include <porteiro2/porteiro2.h>
@@ -46,18 +46,18 @@ const int angulos_default[3] = {50,90,90};
 // const int angulos_default[3] = {60,87,90};
 
 
-// #include <celeste/celeste1.h>
-// #include <celeste/celeste2.h>
-// #include <celeste/celeste3.h>
-// #include <celeste/celeste4.h>
-// #include <celeste/celeste5.h>
+#include <celeste/celeste1.h>
+#include <celeste/celeste2.h>
+#include <celeste/celeste3.h>
+#include <celeste/celeste4.h>
+#include <celeste/celeste5.h>
 
-// // Defina os pinos para os servos
-// const int pinOlho = 10;     //celeste
-// const int pinBoca = 9;      //celeste
-// const int pinPescoco = 11;  //celeste
+// Defina os pinos para os servos
+const int pinOlho = 10;     //celeste
+const int pinBoca = 9;      //celeste
+const int pinPescoco = 11;  //celeste
 
-// const int angulos_default[3] = {50,90,80};
+const int angulos_default[3] = {40,90,80};
 
 // #include <celeste2/celeste1.h>
 // #include <celeste2/celeste2.h>
@@ -72,7 +72,7 @@ const int angulos_default[3] = {50,90,90};
 
 // const int angulos_default[3] = {170,125,85}; // BOCA - OLHO - PESCOCO
 
-const int num_cena =6;
+const int num_cena = 5;
 const int delay_cena = 15000;
 
 Servo olho;
@@ -80,15 +80,15 @@ Servo boca;
 Servo pescoco;
 
 // ===== WTV020SD16P =====
-// static const uint8_t resetPin = 4;   // The pin number of the reset pin.
-// static const uint8_t clockPin = 5;   // The pin number of the clock pin.
-// static const uint8_t dataPin = 6;    // The pin number of the data pin.
-// static const uint8_t busyPin = 7;    // The pin number of the busy pin.
-// WTV020SD16P wtv020sd16p(resetPin, clockPin, dataPin, busyPin);
+static const uint8_t resetPin = 4;   // The pin number of the reset pin.
+static const uint8_t clockPin = 5;   // The pin number of the clock pin.
+static const uint8_t dataPin = 6;    // The pin number of the data pin.
+static const uint8_t busyPin = 7;    // The pin number of the busy pin.
+WTV020SD16P wtv020sd16p(resetPin, clockPin, dataPin, busyPin);
 
 // ===== DFPlayer Mini (Comentado) =====
-SoftwareSerial mySoftwareSerial(3, 2); // RX, TX
-DFRobotDFPlayerMini myDFPlayer;
+// SoftwareSerial mySoftwareSerial(3, 2); // RX, TX
+// DFRobotDFPlayerMini myDFPlayer;
 
 int n, t, cena = 0;
 
@@ -101,22 +101,22 @@ int anguloPescoco = angulos_default[2];
 void setup() 
 {
     // ===== WTV020SD16P Initialization =====
-    // wtv020sd16p.reset();
+    wtv020sd16p.reset();
   
   Serial.begin(9600);
   Serial.println("Inicializando");
   delay(1000);
   
     // ===== DFPlayer Mini Initialization (Comentado) =====
-    mySoftwareSerial.begin(9600);
-    if (!myDFPlayer.begin(mySoftwareSerial)) {
-      //Serial.println(F("DFPlayer Mini nao iniciou corretamente!"));
-      while(1);
-    }
+    // mySoftwareSerial.begin(9600);
+    // if (!myDFPlayer.begin(mySoftwareSerial)) {
+    //   //Serial.println(F("DFPlayer Mini nao iniciou corretamente!"));
+    //   while(1);
+    // }
     //Serial.println(F("DFPlayer Mini inicializado com sucesso"));
   
     // Configurações do DFPlayer (comentadas)
-    myDFPlayer.volume(25); // Define volume (0-30)
+    // myDFPlayer.volume(25); // Define volume (0-30)
     //myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
     //myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
   
@@ -164,17 +164,17 @@ void loop() {
             nframes = nFrames5;
             millis_frames = millis_frames5;
             break;
-        case 6:
-            nframes = nFrames6;
-            millis_frames = millis_frames6;
-            break;
+        // case 6:
+        //     nframes = nFrames6;
+        //     millis_frames = millis_frames6;
+        //     break;
         
         default:
             break;
         }
         
-        // ===== DFPlayer play =====
-        myDFPlayer.play(cena+1);
+        // ===== WTV020SD16P play =====
+        wtv020sd16p.asyncPlayVoice(cena);
         
         // delay(10);
         t=millis();
@@ -209,11 +209,11 @@ void loop() {
                 anguloOlho = pgm_read_word(&frames5[n][1]);
                 anguloPescoco = pgm_read_word(&frames5[n][2]);
                 break;
-            case 6:
-                anguloBoca = pgm_read_word(&frames6[n][0]);
-                anguloOlho = pgm_read_word(&frames6[n][1]);
-                anguloPescoco = pgm_read_word(&frames6[n][2]);
-                break;
+            // case 6:
+            //     anguloBoca = pgm_read_word(&frames6[n][0]);
+            //     anguloOlho = pgm_read_word(&frames6[n][1]);
+            //     anguloPescoco = pgm_read_word(&frames6[n][2]);
+            //     break;
             
             default:
                 anguloBoca = angulos_default[0];
@@ -238,8 +238,8 @@ void loop() {
         // Serial.println(millis_frames);
         // delay(5000);
         
-        // ===== DFPlayer stop (opcional) =====
-        myDFPlayer.pause();
+        // ===== WTV020SD16P stop (opcional) =====
+        wtv020sd16p.stopVoice();
         
         delay(delay_cena);
     }
